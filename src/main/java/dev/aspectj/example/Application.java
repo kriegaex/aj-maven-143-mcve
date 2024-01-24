62e2cb0aa3e0990e2d1ada0cf45c1f13fc5bb7a8
Alexander Kriegisch <Alexander@Kriegisch.name>
2024-01-24 10:18:39 +0700

    Initial commit

---
+++ src/main/java/dev/aspectj/example/Application.java
@@ -0,0 +1,9 @@
+package dev.aspectj.example;
+
+import javax.xml.namespace.QName;
+
+public class Application {
+  public static void main(String[] args) {
+    QName qName= new QName("test");
+  }
+}
